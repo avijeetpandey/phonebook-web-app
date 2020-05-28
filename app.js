9b6eb43f548170d135afd581e1dc90ee3e2b76a9
Avijeet Pandey <40532869+avijeetpandey@users.noreply.github.com>
2020-05-28 22:01:07 +0530

    updated app.js and added server logs

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
 require('dotenv').config();
 const express=require('express');
-const logger=require('morgan')('dev');
+const logger=require('morgan');
 const bodyParser=require('body-parser');
-const path=require('path');
 const app=express();
+const fs=require('fs');
+const path=require('path');
 
-const PORT=process.env.PORT || 3000;
+const PORT=process.env.PORT;
 
 // Routes
 const contactRoutes=require("./routes/contact");
 
 // database connectivity
 const mongoose=require('mongoose');
-const uri="mongodb://localhost:27017/contactsDB";
-mongoose.connect(process.env.MONGODB_URI || uri ,{useNewUrlParser:true,useUnifiedTopology:true});
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true,useUnifiedTopology:true});
 
 
 //using middlewares
 let bodyParserJSON=bodyParser.json();
 let bodyURLEncoded=bodyParser.urlencoded({extended:true});
+// writting stream for logs
+const logStream=fs.createWriteStream(path.join(__dirname,'server.log'),{flags:'a'});
 
-app.use(logger);
+app.use(logger('dev'));
+app.use(logger('combined', { stream: logStream }));
 app.use(bodyParserJSON);
 app.use(bodyURLEncoded);
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static("public"));
 app.set("view engine",'ejs');
 
 //Routes
@@ -39,5 +42,3 @@ app.get('/about',(req,res)=>{
 app.listen(PORT,(req,res)=>{
     console.log(`Server is up and running on port : ${PORT}`)
 })
-
-module.exports=app;
